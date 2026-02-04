6c198da9a7d525544f3fe536ef72fea4a50cc38d
mmansikka <mmansikka>
2026-02-04 13:39:38 +0200

    Complete my first model

--- models/staging/stg_stripe__payments.sql
+++ models/staging/stg_stripe__payments.sql
@@ -6,9 +6,14 @@
 
 with src as (
     select
-        * except (amount),
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+        created as payment_date,
         {{ cents_to_dollars('amount') }} as amount
     from {{ source('stripe', 'stripe_payments')}}
+
 )
 
 select *
