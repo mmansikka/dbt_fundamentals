c8cd06ea6f13564203079d29fac1289a1dbf21e3
mmansikka <mmansikka>
2026-02-05 11:17:53 +0200

    complete course

---
+++ tests/assert_stg_stripe__payment_total_positive.sql
@@ -0,0 +1,12 @@
+with src as (
+    select
+        order_id,
+        sum(amount) as total_amount
+    from {{ ref('stg_stripe__payments') }}
+    group by order_id
+    having total_amount < 0
+
+)
+
+select *
+from src
